3b5875a0380f8c8c3b69e20c09231151da28c276
shiva508 <dasarishiva1@gmail.com>
2022-06-08 09:01:26 +0530

    security filter to validate token

--- student-pool-apigateway/src/main/java/com/pool/filter/LoggingFilter.java
+++ student-pool-apigateway/src/main/java/com/pool/filter/LoggingFilter.java
@@ -1,9 +1,12 @@
 package com.pool.filter;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -14,6 +17,12 @@ public class LoggingFilter implements GlobalFilter {
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		HttpHeaders headers = exchange.getRequest().getHeaders();
+		List<String> shivaHeader = headers.get("Shiva");
+		if(null !=shivaHeader && !shivaHeader.isEmpty()) {
+			logger.info("current request header is --------->{}", shivaHeader);
+		}
+		
 		logger.info("current request url is --------->{}", exchange.getRequest().getPath());
 		return chain.filter(exchange);
 	}
